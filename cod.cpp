d088f3f6a1c4147307d2704ac63bbc2543ed0187
MarinaPl <plautina95@mail.ru>
2017-04-29 05:15:34 +0300

    work

--- cod.cpp
+++ cod.cpp
@@ -4,22 +4,48 @@
 #include <algorithm>
 #include <iomanip> 
 #include <vector>
+#include <cstdlib>
+
 using namespace std;
 int N;
 float m, q, sigma_l, sigma_h;
 vector<int> ans;
 
-void fill(int n, string s) {
+void fill(int n) {
+	int k1 = pow(2, n);  //2^N
 	ofstream file2("output.txt");
+
+	
+	for (int i = 0; i < k1; i++) {
+		int j = i;
+		int k3 = 0;
+		file2.fill('0'); 
+		while (j != 0)
+		{	
+			file2 << j % 2;
+			j /= 2;
+			k3++;
+		}
+		file2 << setw(N - k3);
+		file2 << j << endl;
+		//file2 << k3<< endl;
+		
+		
+	}
+}
+
+
+	/*
+	void fill(int n, string s) {
 	if (n == 0) {
 		file2 << s << endl;
 	}
 	else {
 		fill(n - 1, s + "0");
 		fill(n - 1, s + "1");
-	}
-	file2.close();	
-}
+	}*/
+
+//	file2.close();
 
 
 int main() {
@@ -27,45 +53,11 @@ int main() {
 	file.open("input.txt");
 	file >> N;
 	file.close();
-	fill(N, "");
-
+	fill(N);
+	return 0;
+	//заполнили промежуточный файл всеми комбинациями, теперь нужно из него вытащить каждый 0 и 1 в коэфф.
 }
 
-/*
-
-void main() {
-	int j, K;
-	string s1, s2;
-
-	fstream file;
-	file.open("input.txt");
-	file >> N;
-	file.close();
-
-	ofstream file2("output.txt");
-	
-	/*s1 = '0';
-
-	for (int i = 0; i < N; i++) {
-		s1 = s1 + '0';
-		file2 << s1 << endl;
-	}
-	*/
-
-/*
-	for (int i = 0; i < N; i++) {
-		j = i;
-		while (j > 0)
-		{
-			file2 << setw(2) << setfill('0') << j % 2;
-			j /= 2 ;
-		}
-		file2 <<  endl;
-}
-
-	file2.close();
 
 
-}
-
-*/
+ 
