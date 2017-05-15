0766a4a67ca329b20e3218c5f38c798d6278fe1b
MarinaPl <plautina95@mail.ru>
2017-05-16 02:31:02 +0300

    проблема с запоминанием векторов

--- cod.cpp
+++ cod.cpp
@@ -13,6 +13,7 @@ int N;
 float m,S, K, q, sigma_l, sigma_h;
 vector < vector<float> > p;//матрица переходных вероятностей probability
 vector < vector<int> >  koef; //-+1
+vector<int> ind;  //вспомогательный для всех возможных волатильностей
 vector<float> vol;//волатильность  - все возможные сигмы
 vector <float> help; //чтобы облегчить себе жизнь при подсчете матожидания
 
@@ -23,6 +24,7 @@ void read_task() {
 	//выделение памяти под векторы и матрицы
 	p = vector < vector<float> >(N, vector<float>(N, 0));
 	koef = vector < vector<int> >(pow(2, N), vector<int>(N, 0));
+	ind = vector<int>(N);
 	vol = vector<float>(N);
 	//чтение матрицы переходных вероятностей
 	for (int i = 0; i < N; i++)
@@ -88,6 +90,21 @@ void fill_vol(float s1, float s2) {
 		vol[i] = vol[i - 1] + i*h;
 	}
 }
+//перебираем все векторы волатильностей - шаманство, тут именно индексы   - 	comb(N, 0); - вызов
+void comb(int n, int t){
+	ofstream f2("fuck.txt");
+
+	if (t == n){
+		for (int i = 0; i < n; i++)
+			f2 << ind[i] << " ";
+		f2 << endl;
+		return;
+	}
+	for (int i = 1; i <= n; i++){
+		ind[t] = i;
+		comb(n, t + 1);
+	}
+}
 //функция нахождения максимума - чтобы проще было жить
 float f(float x) {
 	double res = x - K;
@@ -126,6 +143,8 @@ int main() {
 	fill_koef(N); //матрица
 	fill_vol(sigma_l, sigma_h); //вектор волатильностей
 	calculation(vol); //собственно вычисления + запись результата в файл
+	comb(N, 0);
+	system("pause");
 	return 0;
 }
 
